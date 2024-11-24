5b59027d03db312d7c5a9816a75e1cd68e9e7716
nestoraima230 <nestoraima_22@alu.uabcs.mx>
2024-11-23 17:21:47 -0700

    testing endpoint

--- server.js
+++ server.js
@@ -1,19 +1,34 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const connection = require('./db'); 
+
 const app = express();
-
-require('./db');
-
 
 app.use(cors());
 app.use(bodyParser.json());
 
+//Ruta raiz
 app.get('/', (req, res) => {
     res.send('Server is running');
 });
 
+// Endpoint de prueba
+app.get('/test-users', (req, res) => {
+    const query = 'SELECT * FROM Users';
+
+    connection.query(query, (err, results) => {
+      if (err) {
+        console.error('Error en la consulta:', err);
+        return res.status(500).json({ error: 'Error al consultar la base de datos' });
+      }
+      
+      res.json(results);
+    });
+});
+  
+
 const PORT = process.env.PORT || 3000; 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-});
+}); 
