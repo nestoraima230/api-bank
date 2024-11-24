a46369de52984bef3115d83c59dc7607e51579c6
nestoraima230 <nestoraima_22@alu.uabcs.mx>
2024-11-24 15:02:32 -0700

    Fixing problems

--- db.js
+++ db.js
@@ -1,14 +1,12 @@
 const mysql = require('mysql2');
-const url = require('url');
 require('dotenv').config();
-
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  port: process.env.DB_PORT || 3306 
 });
 
 connection.connect((err) => {
@@ -20,4 +18,3 @@ connection.connect((err) => {
 });
 
 module.exports = connection;
-
--- server.js
+++ server.js
@@ -1,33 +1,32 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const connection = require('./db'); 
+const connection = require('./db'); // Aquí usas la conexión a la base de datos
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 
-
 app.get('/', (req, res) => {
-    res.send('Server is running');
+  res.send('Server is running');
 });
 
 // Endpoint de prueba
 app.get('/test-users', (req, res) => {
-    const query = 'SELECT * FROM Users'; 
+  const query = 'SELECT * FROM Users'; 
 
-    connection.query(query, (err, results) => {
-      if (err) {
-        console.error('Error en la consulta:', err);
-        return res.status(500).json({ error: 'Error al consultar la base de datos' });
-      }
-      
-      res.json(results);
-    });
+  connection.query(query, (err, results) => {
+    if (err) {
+      console.error('Error en la consulta:', err);
+      return res.status(500).json({ error: 'Error al consultar la base de datos' });
+    }
+
+    res.json(results);
+  });
 });
 
-const PORT = process.env.PORT || 3003;
+const PORT = process.env.PORT || 3003;  
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
