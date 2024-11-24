a255b609ab1c61995acb3f6c3fdfbc4e9c52a4c3
nestoraima230 <nestoraima_22@alu.uabcs.mx>
2024-11-24 10:13:03 -0700

    Change db.js

--- db.js
+++ db.js
@@ -1,14 +1,22 @@
 const mysql = require('mysql2');
-require('dotenv').config();  
+const url = require('url');
+require('dotenv').config();
 
-const connection = mysql.createConnection(process.env.DATABASE_URL);
+// Analizar la URL de la base de datos
+const dbUrl = url.parse(process.env.DATABASE_URL);
+
+const connection = mysql.createConnection({
+  host: dbUrl.hostname,
+  user: dbUrl.auth.split(':')[0],  // El nombre de usuario (antes de ':')
+  password: dbUrl.auth.split(':')[1],  // La contraseña (después de ':')
+  database: dbUrl.pathname.split('/')[1],  // El nombre de la base de datos (después de '/')
+  port: dbUrl.port // El puerto
+});
 
 connection.connect((err) => {
   if (err) {
     console.error('Failed to connect to database:', err);
     return;
   }
-  console.log('Connected to the MySQL database');
+  console.log('Successfully connected to the MySQL database');
 });
-
-module.exports = connection;
