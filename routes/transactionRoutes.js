b04f8b5e5c23c71ef9023c7dcdf32bf0287afa19
nestoraima230 <nestoraima_22@alu.uabcs.mx>
2024-12-06 11:09:47 -0700

    Update transactionRoutes

--- routes/transactionRoutes.js
+++ routes/transactionRoutes.js
@@ -4,6 +4,6 @@ const TransactionController = require('../controllers/transactionController');
 
 
 router.post('/transfer', TransactionController.transferMoney); // POST /api/transaction/transfer
-router.get('/', TransactionController.getTransactionsByUser);  // GET /api/transaction/           
+router.get('/:userId', TransactionController.getTransactionsByUser);  // GET /api/transaction/1           
 
 module.exports = router;
